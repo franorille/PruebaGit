1c3693c3750898adf4bea0a6359df4f8de79c280
Francisco <franorille@gmail.com>
2023-05-29 19:22:57 +0200

    ultimo commit desde rama 2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Esto es la primera linea");
         System.out.println("Esta es la segunda linea");
         System.out.println("Nueva linea 3");
+        System.out.println("Esto es una nueva linea que estoy creando desde la rama 2");
     }
 }
